04d0c702062d400209b1f4496ee750c7948d740f
Lebohang Motaung <lebohangmotaung88@gmail.com>
2022-06-03 13:02:56 -0700

    My variables tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+
+int main(void)
+
+{
+	char alphabet;
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			putchar(alphabet);
+		}
+
+	putchar('\n');
+
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
