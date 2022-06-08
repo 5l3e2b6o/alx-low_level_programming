eb3dd5aef9fdc719c0d909730f66a40b18a746b1
Lebohang Motaung <lebohangmotaung88@gmail.com>
2022-06-08 13:25:12 -0700

    lowercase file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * islower -> checks if is lowercase
+ *
+ * Return: always 1 or 0 depending on condition
+ * */
+int _islower(int c)
+{
+	if(c > 'a' &&c < 'z')
+	{
+		return(1)
+	}
+
+	else
+	{
+		return (0)
+	}
+
+}
