b0c60aeba77d9aee4ad6ec29f3fe67dbf9f12920
Lebohang Motaung <lebohangmotaung88@gmail.com>
2022-06-08 05:23:57 -0700

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#includes "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: always 0.
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; j++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
