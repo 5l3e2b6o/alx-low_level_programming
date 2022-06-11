269b28adb3b9ce6f58578325d8c3ab774a4b6144
Lebohang Motaung <lebohangmotaung88@gmail.com>
2022-06-11 07:41:08 -0700

    my uppercase character tast

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a function that checks uppercase alphabet
+ * @c: an integer paramer
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
